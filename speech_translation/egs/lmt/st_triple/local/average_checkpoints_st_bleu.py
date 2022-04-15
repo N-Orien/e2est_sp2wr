53ce3b4923ea3d05591a3abf5cb0483ec17b1b67
zd-yang <81670041+N-Orien@users.noreply.github.com>
2022-04-16 03:30:51 +0900

    Add files via upload

---
+++ speech_translation/egs/lmt/st_triple/local/average_checkpoints_st_bleu.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import json
+import os
+
+import numpy as np
+
+
+def main():
+    if args.log is not None:
+        with open(args.log) as f:
+            logs = json.load(f)
+        val_scores = []
+        for log in logs:
+            if "validation/main/bleu" in log.keys():
+                val_scores += [[log["epoch"], log["validation/main/bleu"]]]
+        if len(val_scores) == 0:
+            raise ValueError("`validation/main/bleu` is not found in log.")
+        val_scores = np.array(val_scores)
+
+        if args.num == 1:
+            idx_max = np.argmax(val_scores, axis=0)[-1]
+            max_val_score = val_scores[idx_max]
+            print(f"best val score: {max_val_score[1]}")
+            print(f"best val score reached at ep: {int(max_val_score[0])}")
+            last = [os.path.join(os.path.dirname(args.snapshots[0]), f"snapshot.ep.{int(max_val_score[0])}")]
+        elif args.num > 1:
+            sort_idx = np.argsort(val_scores[:, -1])
+            sorted_val_scores = val_scores[sort_idx][::-1]
+            print("best val scores = " + str(sorted_val_scores[:args.num, 1]))
+            print("selected epochs = " + str(sorted_val_scores[:args.num, 0].astype(np.int64)))
+            last = [os.path.dirname(args.snapshots[0]) + "/snapshot.ep.%d" % (
+                int(epoch)) for epoch in sorted_val_scores[:args.num, 0]]
+        else:
+            raise NotImplementedError
+    else:
+        last = sorted(args.snapshots, key=os.path.getmtime)
+        last = last[-args.num:]
+    avg = None
+
+    existing_last = []
+    for path in last:
+        if os.path.exists(path):
+            existing_last.append(path)
+    last = existing_last
+    args.num = len(last)
+    print(f"averaging over {args.num} checkpoints: {[s.split('/')[-1] for s in last]}")
+
+    if args.backend == 'pytorch':
+        import torch
+
+        if len(last) == 1:
+            print(f"Get the model with best val BLEU")
+            avg = torch.load(last[0], map_location=torch.device("cpu"))["model"]
+
+        elif len(last) > 1:
+            # sum
+            for path in last:
+                states = torch.load(path, map_location=torch.device("cpu"))["model"]
+                if avg is None:
+                    avg = states
+                else:
+                    for k in avg.keys():
+                        avg[k] += states[k]
+
+            # average
+            for k in avg.keys():
+                if avg[k] is not None:
+                    avg[k] /= args.num
+        else:
+            raise NotImplementedError
+        
+        # Save trans model
+        print(f"Save trans_model to {args.out}")
+        torch.save(avg, args.out)
+
+    elif args.backend == 'chainer':
+        # sum
+        for path in last:
+            states = np.load(path)
+            if avg is None:
+                keys = [x.split('main/')[1] for x in states if 'model' in x]
+                avg = dict()
+                for k in keys:
+                    avg[k] = states['updater/model:main/{}'.format(k)]
+            else:
+                for k in keys:
+                    avg[k] += states['updater/model:main/{}'.format(k)]
+        # average
+        for k in keys:
+            if avg[k] is not None:
+                avg[k] /= args.num
+        np.savez_compressed(args.out, **avg)
+        os.rename('{}.npz'.format(args.out), args.out)  # numpy save with .npz extension
+    else:
+        raise ValueError('Incorrect type of backend')
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='average models from snapshot')
+    parser.add_argument("--snapshots", required=True, type=str, nargs="+")
+    parser.add_argument("--out", required=True, type=str)
+    parser.add_argument("--num", default=10, type=int)
+    parser.add_argument("--backend", default='chainer', type=str)
+    parser.add_argument("--log", default=None, type=str, nargs="?")
+    return parser
+
+
+if __name__ == '__main__':
+    args = get_parser().parse_args()
+    main()
---
+++ speech_translation/egs/lmt/st_triple/local/data_prep.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+# Copyright 2019 Kyoto University (Hirofumi Inaguma)
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+export LC_ALL=C
+
+. utils/parse_options.sh || exit 1;
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <src-dir>"
+    echo "e.g.: $0 dataset_path"
+    exit 1;
+fi
+
+for set in train dev test; do
+    src=$1/data/${set}
+    dst=data/local/${set}
+
+    [ ! -d ${src} ] && echo "$0: no such directory ${src}" && exit 1;
+
+    wav_dir=${src}/wav
+    trans_dir=${src}/txt
+    yml=${trans_dir}/${set}.yaml
+    en=${trans_dir}/${set}.en
+    sp=${trans_dir}/${set}.sp
+    wr=${trans_dir}/${set}.wr
+
+    mkdir -p ${dst} || exit 1;
+
+    [ ! -d ${wav_dir} ] && echo "$0: no such directory ${wav_dir}" && exit 1;
+    [ ! -d ${trans_dir} ] && echo "$0: no such directory ${trans_dir}" && exit 1;
+    [ ! -f ${yml} ] && echo "$0: expected file ${yml} to exist" && exit 1;
+    [ ! -f ${en} ] && echo "$0: expected file ${en} to exist" && exit 1;
+    [ ! -f ${sp} ] && echo "$0: expected file ${sp} to exist" && exit 1;
+    [ ! -f ${wr} ] && echo "$0: expected file ${wr} to exist" && exit 1;
+
+    wav_scp=${dst}/wav.scp; [[ -f "${wav_scp}" ]] && rm ${wav_scp}
+    trans_en=${dst}/text.en; [[ -f "${trans_en}" ]] && rm ${trans_en}
+    trans_sp=${dst}/text.sp; [[ -f "${trans_sp}" ]] && rm ${trans_sp}
+    trans_wr=${dst}/text.wr; [[ -f "${trans_wr}" ]] && rm ${trans_wr}
+    utt2spk=${dst}/utt2spk; [[ -f "${utt2spk}" ]] && rm ${utt2spk}
+    spk2utt=${dst}/spk2utt; [[ -f "${spk2utt}" ]] && rm ${spk2utt}
+    segments=${dst}/segments; [[ -f "${segments}" ]] && rm ${segments}
+
+    # error check
+    n=$(cat ${yml} | grep duration | wc -l)
+    n_en=$(cat ${en} | wc -l)
+    n_sp=$(cat ${sp} | wc -l)
+    n_wr=$(cat ${wr} | wc -l)
+    [ ${n} -ne ${n_en} ] && echo "Warning: expected ${n} en data files, found ${n_en}" && exit 1;
+    [ ${n} -ne ${n_sp} ] && echo "Warning: expected ${n} sp data data files, found ${n_sp}" && exit 1;
+    [ ${n} -ne ${n_wr} ] && echo "Warning: expected ${n} wr data data files, found ${n_wr}" && exit 1;
+
+
+    # (1a) Transcriptions and translations preparation
+    # make basic transcription file (add segments info)
+    cp ${yml} ${dst}/.yaml0
+    grep duration ${dst}/.yaml0 > ${dst}/.yaml1
+    awk '{
+        duration=$3; offset=$5; spkid=$7; wavfile=$9
+        gsub(",","",duration);
+        gsub(",","",offset);
+        gsub(",","",spkid);
+	gsub("}","",wavfile);
+        duration=sprintf("%.7f", duration);
+        if ( duration < 0.2 ) extendt=sprintf("%.7f", (0.2-duration)/2);
+        else extendt=0;
+        offset=sprintf("%.7f", offset);
+        startt=offset-extendt;
+        endt=offset+duration+extendt;
+        printf("lmt_%s@%s@%07.0f@%07.0f\n", spkid, wavfile, int(1000*startt+0.5), int(1000*endt+0.5));
+    }' ${dst}/.yaml1 > ${dst}/.yaml2
+    # NOTE: Extend the lengths of short utterances (< 0.2s) rather than exclude them
+
+    cp ${en} ${dst}/en.org
+    cp ${sp} ${dst}/sp.org
+    cp ${wr} ${dst}/wr.org
+
+    for lang in sp wr en; do
+        # normalize punctuation
+        normalize-punctuation.perl -l ${lang} < ${dst}/${lang}.org > ${dst}/${lang}.norm
+
+        # lowercasing
+        lowercase.perl < ${dst}/${lang}.norm > ${dst}/${lang}.norm.lc
+        cp ${dst}/${lang}.norm ${dst}/${lang}.norm.tc
+
+        # remove punctuation
+        local/remove_punctuation.pl < ${dst}/${lang}.norm.lc > ${dst}/${lang}.norm.lc.rm
+
+        # tokenization
+        tokenizer.perl -l ${lang} -q < ${dst}/${lang}.norm.tc > ${dst}/${lang}.norm.tc.tok
+        tokenizer.perl -l ${lang} -q < ${dst}/${lang}.norm.lc > ${dst}/${lang}.norm.lc.tok
+        tokenizer.perl -l ${lang} -q < ${dst}/${lang}.norm.lc.rm > ${dst}/${lang}.norm.lc.rm.tok
+
+        paste -d " " ${dst}/.yaml2 ${dst}/${lang}.norm.tc.tok | sort > ${dst}/text.tc.${lang}
+        paste -d " " ${dst}/.yaml2 ${dst}/${lang}.norm.lc.tok | sort > ${dst}/text.lc.${lang}
+        paste -d " " ${dst}/.yaml2 ${dst}/${lang}.norm.lc.rm.tok | sort > ${dst}/text.lc.rm.${lang}
+
+        # save original and cleaned punctuation
+        lowercase.perl < ${dst}/${lang}.org | text2token.py -s 0 -n 1 | tr " " "\n" \
+            | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' > ${dst}/punctuation.${lang}
+        lowercase.perl < ${dst}/${lang}.norm.tc | text2token.py -s 0 -n 1 | tr " " "\n" \
+            | sort | uniq | grep -v -e '^\s*$' | awk '{print $0 " " NR+1}' > ${dst}/punctuation.clean.${lang}
+    done
+
+
+    # error check
+    n=$(cat ${dst}/.yaml2 | wc -l)
+    n_en=$(cat ${dst}/en.norm.tc.tok | wc -l)
+    n_sp=$(cat ${dst}/sp.norm.tc.tok | wc -l)
+    n_wr=$(cat ${dst}/wr.norm.tc.tok | wc -l)
+    [ ${n} -ne ${n_en} ] && echo "Warning: expected ${n} data data files, found ${n_en}" && exit 1;
+    [ ${n} -ne ${n_sp} ] && echo "Warning: expected ${n} data data files, found ${n_sp}" && exit 1;
+    [ ${n} -ne ${n_wr} ] && echo "Warning: expected ${n} data data files, found ${n_wr}" && exit 1;
+
+
+    # (1c) Make segments files from transcript
+    #segments file format is: utt-id start-time end-time, e.g.:
+    #ku_00001_0003501_0003684 ku_0001 003.501 0003.684
+    awk '{
+        segment=$1; split(segment,s,"[@]");
+        fileid=s[1] "@" s[2]; startf=s[3]; endf=s[4];
+        printf("%s %s %.2f %.2f\n", segment, fileid, startf/1000, endf/1000);
+    }' < ${dst}/text.tc.en | uniq | sort > ${dst}/segments
+
+    awk '{
+        segment=$1; split(segment,s,"[@]");
+        fileid=s[1] "@" s[2]; wavfile=s[2]
+        printf("%s cat '${wav_dir}'/%s |\n", fileid, wavfile);
+    }' < ${dst}/text.tc.en | uniq | sort > ${dst}/wav.scp
+
+    awk '{
+        segment=$1; split(segment,s,"[@]");
+        spkid=s[1]; print $1 " " spkid
+    }' ${dst}/segments | uniq | sort > ${dst}/utt2spk
+
+    cat ${dst}/utt2spk | utils/utt2spk_to_spk2utt.pl | sort > ${dst}/spk2utt
+
+    # error check
+    n_en=$(cat ${dst}/text.tc.en | wc -l)
+    n_sp=$(cat ${dst}/text.tc.sp | wc -l)
+    n_wr=$(cat ${dst}/text.tc.wr | wc -l)
+    [ ${n_sp} -ne ${n_wr} ] && echo "Warning: expected ${n_sp} wr data files, found ${n_wr}" && exit 1;
+    [ ${n_wr} -ne ${n_en} ] && echo "Warning: expected ${n_wr} en data files, found ${n_en}" && exit 1;
+
+    # Copy stuff into its final locations [this has been moved from the format_data script]
+    mkdir -p data/${set}
+
+    # remove duplicated utterances (the same offset)
+    echo "remove duplicate lines..."
+    cut -d ' ' -f 1 ${dst}/text.tc.en | sort | uniq -c | sort -n -k1 -r | grep -v '1 lmt' \
+        | sed 's/^[ \t]*//' > ${dst}/duplicate_lines
+    cut -d ' ' -f 1 ${dst}/text.tc.en | sort | uniq -c | sort -n -k1 -r | grep '1 lmt' \
+        | cut -d '1' -f 2- | sed 's/^[ \t]*//' > ${dst}/reclist
+    reduce_data_dir.sh ${dst} ${dst}/reclist data/${set}
+    for l in en sp wr; do
+        for case in tc lc lc.rm; do
+            cp ${dst}/text.${case}.${l} data/${set}/text.${case}.${l}
+        done
+    done
+    utils/fix_data_dir.sh --utt_extra_files \
+        "text.tc.en text.lc.en text.lc.rm.en text.tc.sp text.lc.sp text.lc.rm.sp text.tc.wr text.lc.wr text.lc.rm.wr" \
+        data/${set}
+
+    # error check
+    n_seg=$(cat data/${set}/segments | wc -l)
+    n_text=$(cat data/${set}/text.tc.en | wc -l)
+    [ ${n_seg} -ne ${n_text} ] && echo "Warning: expected ${n_seg} data data files, found ${n_text}" && exit 1;
+
+    echo "$0: successfully prepared data in ${dst}"
+done
